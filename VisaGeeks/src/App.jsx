352d402879480d1132d79e50f0d1fed0d5e9d8fc
abdelali <oukarriabdelali@gmail.com>
2025-07-03 23:01:04 +0100

    create HomePgae and Footer

--- VisaGeeks/src/App.jsx
+++ VisaGeeks/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import FooterComponent from './component/footer.jsx'
+import HeaderComponent from './component/header.jsx'
 
 function App() {
  
   return (
     <>
-      <h1>hellow</h1>
+      <HeaderComponent/>
+      <FooterComponent/>
     </>
   )
 }
---
+++ VisaGeeks/src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import FooterComponent from '../component/footer'
+import HeaderComponent from '../component/header'
+
+const Home = () => {
+  return (
+    <>
+        <HeaderComponent/>
+        
+    </>
+  )
+}
+
+export default Home
